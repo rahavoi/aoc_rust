cc7776f89a998d11e3a61fd6a7afa6e3dceb2c7b
rahavoi <irahavoi@gmail.com>
2024-01-28 15:33:03 -0500

    Optimized day 9

--- src/_2015_day9.rs
+++ src/_2015_day9.rs
@@ -3,54 +3,42 @@ use std::collections::HashMap;
 
 use itertools::Itertools;
 
-pub struct DistancesData {
-    distances : HashMap<String, HashMap<String, i32>>,
-    longest_from: String,
-    longest_to: String
-}
-
 pub fn solve(input : &String) {
     let mut min_distance =  i32::MAX;
-    let distances_data = init_map(input);
-    let distances = distances_data.distances;
+    let distances = init_map(input);
 
-    for path in distances.keys().into_iter().permutations(distances.len()) {
+    let mut cities : Vec<&String> = distances.keys().collect();
+    let a_city = cities.remove(0);
+
+    for path in cities.into_iter().permutations(distances.len()) {
         //println!("{:?}", path);
         let mut distance = 0;
+        let mut max_distance = i32::MIN;
 
         let mut it = path.iter().peekable();
-        let mut copy = it.clone();
-        let mut avoid_longest_commute = true;
 
-        //Trick here is to completely avoid paths containing the longest commute between a pair of cities
-        //determined earlier, as such paths are guaranteed to be suboptimal.
-        //This way we reduce the number of permutations we need to check.
-        //Assumption: checking if a path contains a pair of cities with longest commute
-        //is cheaper than calculating the total travelling distance of the path.
-        while let Some(city) = copy.next() {
-            if copy.peek().is_some() {
-                let next = copy.peek().unwrap() as &String;
-
-                if(*city == &distances_data.longest_to || *city == &distances_data.longest_from) &&
-                    (next == &distances_data.longest_to || next == &distances_data.longest_from) {
-                    avoid_longest_commute = false;
-                    break
-                }
-            }
-        }
-
-        if !avoid_longest_commute {
-            continue;
-        }
+        let first = it.peek().unwrap() as &String;
+        distance = distance + distances.get(a_city).unwrap().get(first).unwrap();
 
         while let Some(city) = it.next() {
             if it.peek().is_some() {
                 let next = it.peek().unwrap() as &String;
-                distance = distance + distances
+
+                let cur_distance = distances
                     .get(*city).unwrap()
                     .get(next).unwrap();
+
+                max_distance = cmp::max(max_distance, cur_distance.clone());
+
+                distance = distance + cur_distance;
+            } else {
+                let cur_distance = distances.get(*city).unwrap().get(a_city).unwrap();
+                max_distance = cmp::max(max_distance, cur_distance.clone());
+                distance = distance + cur_distance;
             }
         }
+
+        distance = distance - max_distance;
 
         min_distance = cmp::min(distance, min_distance);
     }
@@ -58,27 +46,15 @@ pub fn solve(input : &String) {
     println!("{}", min_distance);
 }
 
-fn init_map(input: &String) -> DistancesData {
+fn init_map(input: &String) -> HashMap<String, HashMap<String, i32>> {
     let mut distances: HashMap<String, HashMap<String, i32>> = HashMap::new();
-    let mut longest_distance = i32::MIN;
-    let mut longest_from= "";
-    let mut longest_to= "";
-
 
     input.split("\n").for_each(|l| {
         let mut parts = l.split(" = ");
         let mut cities = parts.next().unwrap().split(" to ");
         let distance = parts.next().unwrap().parse::<i32>().unwrap();
-
         let from = cities.next().unwrap();
         let to = cities.next().unwrap();
-
-        if distance > longest_distance {
-            longest_distance = distance;
-            longest_to = to;
-            longest_from = from;
-
-        }
 
         if distances.get(from).is_none() {
             distances.insert(from.to_string(), HashMap::new());
@@ -97,9 +73,5 @@ fn init_map(input: &String) -> DistancesData {
             .insert(from.to_string(), distance.clone());
     });
 
-    DistancesData {
-        distances,
-        longest_from : longest_from.to_string(),
-        longest_to : longest_to.to_string()
-    }
+    distances
 }
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,25 @@
 use std::fs;
 use std::time::Instant;
 
-mod _2015_day3;
+mod _2015_day9;
 
 fn main() {
     let input = parse_input();
     let now = Instant::now();
     //_2015_day1::solve(&input);
     //_2015_day2::solve(&input);
-
-    _2015_day3::solve(&input);
-
+    //_2015_day3::solve(&input);
 
 
 
 
-    //_2015_day9::solve(&input);
+
+    _2015_day9::solve(&input);
 
     println!("Elapsed: {:.2?}", now.elapsed());
 }
 
 fn parse_input() -> String{
-    let path = std::path::Path::new("/Users/irahavoi/IdeaProjects/aoc_rust/resources/2015/Day3.txt");
+    let path = std::path::Path::new("/Users/irahavoi/IdeaProjects/aoc_rust/resources/2015/Day9.txt");
     fs::read_to_string(path).expect("No Such File")
 }
